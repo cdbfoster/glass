7143ca9fefe4f6f5fbd5feea54b5ec5058ad7aff
Chris Foster <cdbfoster@gmail.com>
2014-12-26 17:34:03 -0500

    Implemented X11XCB_DisplayServer's WindowData system.

---
+++ source/glass/displayserver/x11xcb_displayserver/WindowData.cpp
@@ -0,0 +1,304 @@
+/*
+* This file is part of Glass.
+*
+* Glass is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Glass is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Glass. If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2014-2015 Chris Foster
+*/
+
+#include "glass/core/Log.hpp"
+#include "glass/displayserver/x11xcb_displayserver/WindowData.hpp"
+
+using namespace Glass;
+
+WindowData::WindowData(Glass::Window &Window, xcb_window_t ID) :
+	Window(Window),
+	ID(ID)
+{
+
+}
+
+
+ClientWindowData::ClientWindowData(Glass::ClientWindow &Window, xcb_window_t ID, bool NeverFocus) :
+	WindowData(Window, ID),
+	NeverFocus(NeverFocus)
+{
+
+}
+
+
+RootWindowData::RootWindowData(Glass::RootWindow &Window, xcb_window_t ID, uint32_t EventMask) :
+	WindowData(Window, ID),
+	EventMask(EventMask)
+{
+
+}
+
+
+WindowDataContainer::iterator::iterator(WindowToDataMap::iterator const &Base) :
+	Base(Base)
+{
+
+}
+
+
+WindowDataContainer::iterator::iterator(iterator const &Other) :
+	Base(Other.Base)
+{
+
+}
+
+
+WindowDataContainer::iterator &WindowDataContainer::iterator::operator=(iterator const &Other)
+{
+	*this = Other;
+	return *this;
+}
+
+
+bool WindowDataContainer::iterator::operator==(iterator const &Other)	{ return this->Base == Other.Base; }
+bool WindowDataContainer::iterator::operator!=(iterator const &Other)	{ return this->Base != Other.Base; }
+
+
+WindowDataContainer::iterator &WindowDataContainer::iterator::operator++()
+{
+	++this->Base;
+	return *this;
+}
+
+
+WindowDataContainer::iterator WindowDataContainer::iterator::operator++(int)
+{
+	iterator Old = *this;
+
+	++this->Base;
+
+	return Old;
+}
+
+
+WindowDataContainer::iterator &WindowDataContainer::iterator::operator--()
+{
+	--this->Base;
+	return *this;
+}
+
+
+WindowDataContainer::iterator WindowDataContainer::iterator::operator--(int)
+{
+	iterator Old = *this;
+
+	--this->Base;
+
+	return Old;
+}
+
+
+WindowDataContainer::iterator::reference WindowDataContainer::iterator::operator*() const
+{
+	return this->Base->second;
+}
+
+
+WindowDataContainer::iterator::pointer WindowDataContainer::iterator::operator->() const
+{
+	return &this->Base->second;
+}
+
+
+WindowDataContainer::const_iterator::const_iterator(WindowToDataMap::const_iterator const &Base) :
+	Base(Base)
+{
+
+}
+
+
+WindowDataContainer::const_iterator::const_iterator(iterator const &Other) :
+	Base(Other.Base)
+{
+
+}
+
+
+WindowDataContainer::const_iterator::const_iterator(const_iterator const &Other) :
+	Base(Other.Base)
+{
+
+}
+
+
+WindowDataContainer::const_iterator &WindowDataContainer::const_iterator::operator=(const_iterator const &Other)
+{
+	*this = Other;
+	return *this;
+}
+
+
+bool WindowDataContainer::const_iterator::operator==(const_iterator const &Other)	{ return this->Base == Other.Base; }
+bool WindowDataContainer::const_iterator::operator!=(const_iterator const &Other)	{ return this->Base != Other.Base; }
+
+
+WindowDataContainer::const_iterator &WindowDataContainer::const_iterator::operator++()
+{
+	++this->Base;
+	return *this;
+}
+
+
+WindowDataContainer::const_iterator WindowDataContainer::const_iterator::operator++(int)
+{
+	const_iterator Old = *this;
+
+	++this->Base;
+
+	return Old;
+}
+
+
+WindowDataContainer::const_iterator &WindowDataContainer::const_iterator::operator--()
+{
+	--this->Base;
+	return *this;
+}
+
+
+WindowDataContainer::const_iterator WindowDataContainer::const_iterator::operator--(int)
+{
+	const_iterator Old = *this;
+
+	--this->Base;
+
+	return Old;
+}
+
+
+WindowDataContainer::const_iterator::const_reference WindowDataContainer::const_iterator::operator*() const
+{
+	return this->Base->second;
+}
+
+
+WindowDataContainer::const_iterator::const_pointer WindowDataContainer::const_iterator::operator->() const
+{
+	return &this->Base->second;
+}
+
+
+WindowDataContainer::iterator		WindowDataContainer::begin()		{ return this->WindowToData.begin(); }
+WindowDataContainer::const_iterator	WindowDataContainer::begin() const	{ return this->WindowToData.begin(); }
+WindowDataContainer::const_iterator	WindowDataContainer::cbegin() const	{ return this->WindowToData.cbegin(); }
+
+
+WindowDataContainer::iterator		WindowDataContainer::end()			{ return this->WindowToData.end(); }
+WindowDataContainer::const_iterator	WindowDataContainer::end() const	{ return this->WindowToData.end(); }
+WindowDataContainer::const_iterator	WindowDataContainer::cend() const	{ return this->WindowToData.cend(); }
+
+
+bool							WindowDataContainer::empty() const	{ return this->WindowToData.empty(); }
+WindowDataContainer::size_type	WindowDataContainer::size() const	{ return this->WindowToData.size(); }
+
+
+void WindowDataContainer::push_back(value_type &WindowData)
+{
+	if (WindowData == NULL)
+	{
+		LOG_DEBUG_WARNING << "Attempting to add a NULL WindowData!" << std::endl;
+		return;
+	}
+
+	this->WindowToData[&WindowData->Window] = WindowData;
+	this->IDToData[WindowData->ID] = WindowData;
+}
+
+
+WindowDataContainer::size_type WindowDataContainer::erase(Window const *Window)
+{
+	WindowToDataMap::iterator FindData = this->WindowToData.find(Window);
+
+	if (FindData != this->WindowToData.end())
+	{
+		WindowData * const Data = FindData->second;
+
+		this->WindowToData.erase(FindData);
+		this->IDToData.erase(Data->ID);
+
+		delete Data;
+
+		return 1;
+	}
+	else
+		return 0;
+}
+
+
+WindowDataContainer::size_type WindowDataContainer::erase(xcb_window_t ID)
+{
+	IDToDataMap::iterator FindData = this->IDToData.find(ID);
+
+	if (FindData != this->IDToData.end())
+	{
+		WindowData * const Data = FindData->second;
+
+		this->IDToData.erase(FindData);
+		this->WindowToData.erase(&Data->Window);
+
+		delete Data;
+
+		return 1;
+	}
+	else
+		return 0;
+}
+
+
+WindowDataContainer::iterator WindowDataContainer::erase(iterator position)
+{
+	this->IDToData.erase((*position)->ID);
+	return this->WindowToData.erase(position.Base);
+}
+
+
+WindowDataContainer::iterator WindowDataContainer::erase(iterator first, iterator last)
+{
+	for (iterator position = first; position != last; ++position)
+		this->IDToData.erase((*position)->ID);
+
+	return this->WindowToData.erase(first.Base, last.Base);
+}
+
+
+WindowDataContainer::iterator		WindowDataContainer::find(Window const *Window)			{ return this->WindowToData.find(Window); }
+WindowDataContainer::const_iterator	WindowDataContainer::find(Window const *Window) const	{ return this->WindowToData.find(Window); }
+
+
+WindowDataContainer::iterator WindowDataContainer::find(xcb_window_t ID)
+{
+	IDToDataMap::iterator FindData = this->IDToData.find(ID);
+
+	if (FindData != this->IDToData.end())
+		return this->WindowToData.find(&FindData->second->Window);
+	else
+		return this->end();
+}
+
+
+WindowDataContainer::const_iterator	WindowDataContainer::find(xcb_window_t ID) const
+{
+	IDToDataMap::const_iterator FindData = this->IDToData.find(ID);
+
+	if (FindData != this->IDToData.cend())
+		return this->WindowToData.find(&FindData->second->Window);
+	else
+		return this->cend();
+}
---
+++ source/glass/displayserver/x11xcb_displayserver/WindowData.hpp
@@ -0,0 +1,162 @@
+/*
+* This file is part of Glass.
+*
+* Glass is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Glass is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Glass. If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2014-2015 Chris Foster
+*/
+
+#ifndef GLASS_X11XCB_DISPLAYSERVER_WINDOWDATA
+#define GLASS_X11XCB_DISPLAYSERVER_WINDOWDATA
+
+#include <map>
+
+#include <xcb/xcb.h>
+
+#include "glass/core/Window.hpp"
+
+namespace Glass
+{
+	struct WindowData
+	{
+		WindowData(Glass::Window &Window, xcb_window_t ID);
+
+		Glass::Window &Window;
+		xcb_window_t const ID;
+	};
+
+
+	struct ClientWindowData : public WindowData
+	{
+		ClientWindowData(Glass::ClientWindow &Window, xcb_window_t ID, bool NeverFocus);
+
+		bool const NeverFocus;
+	};
+
+
+	struct RootWindowData : public WindowData
+	{
+		RootWindowData(Glass::RootWindow &Window, xcb_window_t ID, uint32_t EventMask);
+
+		uint32_t const EventMask;
+	};
+
+
+	class WindowDataContainer
+	{
+	private:
+		typedef std::map<Window const *, WindowData *>	WindowToDataMap;
+		typedef std::map<xcb_window_t, WindowData *>	IDToDataMap;
+
+	public:
+		typedef size_t				size_type;
+		typedef WindowData *		value_type;
+		typedef value_type &		reference;
+		typedef value_type const &	const_reference;
+
+		class const_iterator;
+
+		class iterator
+		{
+		public:
+			iterator(WindowToDataMap::iterator const &Base);
+			iterator(iterator const &Other);
+
+			typedef WindowDataContainer::value_type	value_type;
+			typedef value_type &					reference;
+			typedef value_type *					pointer;
+			typedef std::bidirectional_iterator_tag	iterator_category;
+
+			iterator   &operator=(iterator const &Other);
+			bool		operator==(iterator const &Other);
+			bool		operator!=(iterator const &Other);
+
+			iterator   &operator++();
+			iterator	operator++(int);
+			iterator   &operator--();
+			iterator	operator--(int);
+
+			reference	operator*() const;
+			pointer		operator->() const;
+
+		private:
+			friend class WindowDataContainer;
+			friend class const_iterator;
+
+			WindowToDataMap::iterator Base;
+		};
+
+		class const_iterator
+		{
+		public:
+			const_iterator(WindowToDataMap::const_iterator const &Base);
+			const_iterator(iterator const &Other);
+			const_iterator(const_iterator const &Other);
+
+			typedef WindowDataContainer::value_type	value_type;
+			typedef value_type &					reference;
+			typedef value_type const &				const_reference;
+			typedef value_type *					pointer;
+			typedef value_type const *				const_pointer;
+			typedef std::bidirectional_iterator_tag	iterator_category;
+
+			const_iterator &operator=(const_iterator const &Other);
+			bool			operator==(const_iterator const &Other);
+			bool			operator!=(const_iterator const &Other);
+
+			const_iterator &operator++();
+			const_iterator	operator++(int);
+			const_iterator &operator--();
+			const_iterator	operator--(int);
+
+			const_reference	operator*() const;
+			const_pointer	operator->() const;
+
+		private:
+			friend class WindowDataContainer;
+
+			WindowToDataMap::const_iterator Base;
+		};
+
+		iterator		begin();
+		const_iterator	begin() const;
+		const_iterator	cbegin() const;
+
+		iterator		end();
+		const_iterator	end() const;
+		const_iterator	cend() const;
+
+		bool		empty() const;
+		size_type	size() const;
+
+		void		push_back(value_type &WindowData);
+		size_type	erase(Window const *Window);
+		size_type	erase(xcb_window_t ID);
+		iterator	erase(iterator position);
+		iterator	erase(iterator first, iterator last);
+
+		iterator		find(Window const *Window);
+		const_iterator	find(Window const *Window) const;
+
+		iterator		find(xcb_window_t ID);
+		const_iterator	find(xcb_window_t ID) const;
+
+	private:
+
+		WindowToDataMap	WindowToData;
+		IDToDataMap		IDToData;
+	};
+}
+
+#endif
