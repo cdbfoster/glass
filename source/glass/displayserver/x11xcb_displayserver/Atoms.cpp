e1d186f3de1d1a64411216bbed8a8532a304f510
Chris Foster <cdbfoster@gmail.com>
2014-12-26 01:24:03 -0500

    Implemented the atom list for X11XCB_DisplayServer.

---
+++ source/glass/displayserver/x11xcb_displayserver/Atoms.cpp
@@ -0,0 +1,189 @@
+/*
+* This file is part of Glass.
+*
+* Glass is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Glass is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Glass. If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2014-2015 Chris Foster
+*/
+
+#include <string>
+#include <vector>
+
+#include "glass/core/Log.hpp"
+#include "glass/displayserver/x11xcb_displayserver/Atoms.hpp"
+
+using namespace Glass;
+
+struct Atom
+{
+	Atom(xcb_atom_t &Data, std::string const &Name) :
+		Data(&Data),
+		Name(Name)
+	{
+
+	}
+
+	xcb_atom_t * const Data;
+	std::string const Name;
+};
+
+
+void Atoms::Initialize(xcb_connection_t *XConnection)
+{
+	std::vector<Atom> Atoms = {
+		{WM_NAME,							"WM_NAME"},
+		{WM_STATE,							"WM_STATE"},
+		{WM_PROTOCOLS,						"WM_PROTOCOLS"},
+		{WM_DELETE_WINDOW,					"WM_DELETE_WINDOW"},
+		{WM_TAKE_FOCUS,						"WM_TAKE_FOCUS"},
+		{_NET_SUPPORTED,					"_NET_SUPPORTED"},
+		{_NET_SUPPORTING_WM_CHECK,			"_NET_SUPPORTING_WM_CHECK"},
+		{_NET_WM_NAME,						"_NET_WM_NAME"},
+		{_NET_WM_VISIBLE_NAME,				"_NET_WM_VISIBLE_NAME"},
+		{_NET_WM_PID,						"_NET_WM_PID"},
+		{_NET_CLIENT_LIST,					"_NET_CLIENT_LIST"},
+		{_NET_CLIENT_LIST_STACKING,			"_NET_CLIENT_LIST_STACKING"},
+		{_NET_ACTIVE_WINDOW,				"_NET_ACTIVE_WINDOW"},
+		{_NET_CLOSE_WINDOW,					"_NET_CLOSE_WINDOW"},
+		{_NET_DESKTOP_VIEWPORT,				"_NET_DESKTOP_VIEWPORT"},
+		{_NET_CURRENT_DESKTOP,				"_NET_CURRENT_DESKTOP"},
+		{_NET_WORKAREA,						"_NET_WORKAREA"},
+		{_NET_VIRTUAL_ROOTS,				"_NET_VIRTUAL_ROOTS"},
+		{_NET_REQUEST_FRAME_EXTENTS,		"_NET_REQUEST_FRAME_EXTENTS"},
+		{_NET_WM_DESKTOP,					"_NET_WM_DESKTOP"},
+		{_NET_WM_WINDOW_TYPE,				"_NET_WM_WINDOW_TYPE"},
+		{_NET_WM_WINDOW_TYPE_DESKTOP,		"_NET_WM_WINDOW_TYPE_DESKTOP"},
+		{_NET_WM_WINDOW_TYPE_DOCK,			"_NET_WM_WINDOW_TYPE_DOCK"},
+		{_NET_WM_WINDOW_TYPE_TOOLBAR,		"_NET_WM_WINDOW_TYPE_TOOLBAR"},
+		{_NET_WM_WINDOW_TYPE_MENU,			"_NET_WM_WINDOW_TYPE_MENU"},
+		{_NET_WM_WINDOW_TYPE_UTILITY,		"_NET_WM_WINDOW_TYPE_UTILITY"},
+		{_NET_WM_WINDOW_TYPE_SPLASH,		"_NET_WM_WINDOW_TYPE_SPLASH"},
+		{_NET_WM_WINDOW_TYPE_DIALOG,		"_NET_WM_WINDOW_TYPE_DIALOG"},
+		{_NET_WM_WINDOW_TYPE_DROPDOWN_MENU,	"_NET_WM_WINDOW_TYPE_DROPDOWN_MENU"},
+		{_NET_WM_WINDOW_TYPE_POPUP_MENU,	"_NET_WM_WINDOW_TYPE_POPUP_MENU"},
+		{_NET_WM_WINDOW_TYPE_TOOLTIP,		"_NET_WM_WINDOW_TYPE_TOOLTIP"},
+		{_NET_WM_WINDOW_TYPE_NOTIFICATION,	"_NET_WM_WINDOW_TYPE_NOTIFICATION"},
+		{_NET_WM_WINDOW_TYPE_COMBO,			"_NET_WM_WINDOW_TYPE_COMBO"},
+		{_NET_WM_WINDOW_TYPE_DND,			"_NET_WM_WINDOW_TYPE_DND"},
+		{_NET_WM_WINDOW_TYPE_NORMAL,		"_NET_WM_WINDOW_TYPE_NORMAL"},
+		{_NET_WM_STATE,						"_NET_WM_STATE"},
+		{_NET_WM_STATE_STICKY,				"_NET_WM_STATE_STICKY"},
+		{_NET_WM_STATE_MAXIMIZED_VERT,		"_NET_WM_STATE_MAXIMIZED_VERT"},
+		{_NET_WM_STATE_MAXIMIZED_HORZ,		"_NET_WM_STATE_MAXIMIZED_HORZ"},
+		{_NET_WM_STATE_FULLSCREEN,			"_NET_WM_STATE_FULLSCREEN"},
+		{_NET_WM_STATE_ABOVE,				"_NET_WM_STATE_ABOVE"},
+		{_NET_WM_STATE_BELOW,				"_NET_NET_WM_STATE_BELOW"},
+		{_NET_WM_STATE_DEMANDS_ATTENTION,	"_NET_WM_STATE_DEMANDS_ATTENTION"},
+		{_NET_WM_STATE_FOCUSED,				"_NET_WM_STATE_FOCUSED"},
+		{_NET_WM_ALLOWED_ACTIONS,			"_NET_WM_ALLOWED_ACTIONS"},
+		{_NET_WM_ACTION_MOVE,				"_NET_WM_ACTION_MOVE"},
+		{_NET_WM_ACTION_RESIZE,				"_NET_WM_ACTION_RESIZE"},
+		{_NET_WM_ACTION_STICK,				"_NET_WM_ACTION_STICK"},
+		{_NET_WM_ACTION_MAXIMIZE_HORZ,		"_NET_WM_ACTION_MAXIMIZE_HORZ"},
+		{_NET_WM_ACTION_MAXIMIZE_VERT,		"_NET_WM_ACTION_MAXIMIZE_VERT"},
+		{_NET_WM_ACTION_FULLSCREEN,			"_NET_WM_ACTION_FULLSCREEN"},
+		{_NET_WM_ACTION_CLOSE,				"_NET_WM_ACTION_CLOSE"},
+		{_NET_WM_ACTION_ABOVE,				"_NET_WM_ACTION_ABOVE"},
+		{_NET_WM_ACTION_BELOW,				"_NET_WM_ACTION_BELOW"},
+		{_NET_WM_STRUT_PARTIAL,				"_NET_WM_STRUT_PARTIAL"},
+		{_NET_FRAME_EXTENTS,				"_NET_FRAME_EXTENTS"},
+		{ATOM,								"ATOM"},
+		{STRING,							"STRING"},
+		{UTF8_STRING,						"UTF8_STRING"}
+	};
+
+	xcb_intern_atom_cookie_t AtomCookies[Atoms.size()];
+	unsigned short Index = 0;
+
+	for (auto &Atom : Atoms)
+		AtomCookies[Index++] = xcb_intern_atom_unchecked(XConnection, false, Atom.Name.length(), Atom.Name.c_str());
+
+	xcb_intern_atom_reply_t *AtomReply;
+
+	Index = 0;
+	for (auto &Atom : Atoms)
+	{
+		if (!(AtomReply = xcb_intern_atom_reply(XConnection, AtomCookies[Index++], NULL)))
+		{
+			LOG_ERROR << "Could not retrieve atom " << Atom.Name << "!" << std::endl;
+			continue;
+		}
+
+		*Atom.Data = AtomReply->atom;
+		free(AtomReply);
+	}
+}
+
+
+// Atom definitions
+
+xcb_atom_t Atoms::WM_NAME;
+xcb_atom_t Atoms::WM_STATE;
+xcb_atom_t Atoms::WM_PROTOCOLS;
+xcb_atom_t Atoms::WM_DELETE_WINDOW;
+xcb_atom_t Atoms::WM_TAKE_FOCUS;
+xcb_atom_t Atoms::_NET_SUPPORTED;
+xcb_atom_t Atoms::_NET_SUPPORTING_WM_CHECK;
+xcb_atom_t Atoms::_NET_WM_NAME;
+xcb_atom_t Atoms::_NET_WM_VISIBLE_NAME;
+xcb_atom_t Atoms::_NET_WM_PID;
+xcb_atom_t Atoms::_NET_CLIENT_LIST;
+xcb_atom_t Atoms::_NET_CLIENT_LIST_STACKING;
+xcb_atom_t Atoms::_NET_ACTIVE_WINDOW;
+xcb_atom_t Atoms::_NET_CLOSE_WINDOW;
+xcb_atom_t Atoms::_NET_DESKTOP_VIEWPORT;
+xcb_atom_t Atoms::_NET_CURRENT_DESKTOP;
+xcb_atom_t Atoms::_NET_WORKAREA;
+xcb_atom_t Atoms::_NET_VIRTUAL_ROOTS;
+xcb_atom_t Atoms::_NET_REQUEST_FRAME_EXTENTS;
+xcb_atom_t Atoms::_NET_WM_DESKTOP;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_DESKTOP;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_DOCK;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_TOOLBAR;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_MENU;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_UTILITY;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_SPLASH;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_DIALOG;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_DROPDOWN_MENU;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_POPUP_MENU;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_TOOLTIP;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_NOTIFICATION;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_COMBO;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_DND;
+xcb_atom_t Atoms::_NET_WM_WINDOW_TYPE_NORMAL;
+xcb_atom_t Atoms::_NET_WM_STATE;
+xcb_atom_t Atoms::_NET_WM_STATE_STICKY;
+xcb_atom_t Atoms::_NET_WM_STATE_MAXIMIZED_VERT;
+xcb_atom_t Atoms::_NET_WM_STATE_MAXIMIZED_HORZ;
+xcb_atom_t Atoms::_NET_WM_STATE_FULLSCREEN;
+xcb_atom_t Atoms::_NET_WM_STATE_ABOVE;
+xcb_atom_t Atoms::_NET_WM_STATE_BELOW;
+xcb_atom_t Atoms::_NET_WM_STATE_DEMANDS_ATTENTION;
+xcb_atom_t Atoms::_NET_WM_STATE_FOCUSED;
+xcb_atom_t Atoms::_NET_WM_ALLOWED_ACTIONS;
+xcb_atom_t Atoms::_NET_WM_ACTION_MOVE;
+xcb_atom_t Atoms::_NET_WM_ACTION_RESIZE;
+xcb_atom_t Atoms::_NET_WM_ACTION_STICK;
+xcb_atom_t Atoms::_NET_WM_ACTION_MAXIMIZE_HORZ;
+xcb_atom_t Atoms::_NET_WM_ACTION_MAXIMIZE_VERT;
+xcb_atom_t Atoms::_NET_WM_ACTION_FULLSCREEN;
+xcb_atom_t Atoms::_NET_WM_ACTION_CLOSE;
+xcb_atom_t Atoms::_NET_WM_ACTION_ABOVE;
+xcb_atom_t Atoms::_NET_WM_ACTION_BELOW;
+xcb_atom_t Atoms::_NET_WM_STRUT_PARTIAL;
+xcb_atom_t Atoms::_NET_FRAME_EXTENTS;
+xcb_atom_t Atoms::ATOM;
+xcb_atom_t Atoms::STRING;
+xcb_atom_t Atoms::UTF8_STRING;
---
+++ source/glass/displayserver/x11xcb_displayserver/Atoms.hpp
@@ -0,0 +1,102 @@
+/*
+* This file is part of Glass.
+*
+* Glass is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Glass is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Glass. If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright 2014-2015 Chris Foster
+*/
+
+#ifndef GLASS_DISPLAYSERVER_X11XCB_DISPLAYSERVER_ATOMS
+#define GLASS_DISPLAYSERVER_X11XCB_DISPLAYSERVER_ATOMS
+
+#include <xcb/xcb.h>
+
+namespace Glass
+{
+	struct Atoms
+	{
+		static xcb_atom_t WM_NAME;
+		static xcb_atom_t WM_STATE;
+
+		static xcb_atom_t WM_PROTOCOLS;
+		static xcb_atom_t WM_DELETE_WINDOW;
+		static xcb_atom_t WM_TAKE_FOCUS;
+
+		static xcb_atom_t _NET_SUPPORTED;
+		static xcb_atom_t _NET_SUPPORTING_WM_CHECK;
+		static xcb_atom_t _NET_WM_NAME;
+		static xcb_atom_t _NET_WM_VISIBLE_NAME;
+		static xcb_atom_t _NET_WM_PID;
+		static xcb_atom_t _NET_CLIENT_LIST;
+		static xcb_atom_t _NET_CLIENT_LIST_STACKING;
+		static xcb_atom_t _NET_ACTIVE_WINDOW;
+		static xcb_atom_t _NET_CLOSE_WINDOW;
+
+		static xcb_atom_t _NET_DESKTOP_VIEWPORT;
+		static xcb_atom_t _NET_CURRENT_DESKTOP;
+		static xcb_atom_t _NET_WORKAREA;
+		static xcb_atom_t _NET_VIRTUAL_ROOTS;
+
+		static xcb_atom_t _NET_REQUEST_FRAME_EXTENTS;
+		static xcb_atom_t _NET_WM_DESKTOP;
+
+		static xcb_atom_t _NET_WM_WINDOW_TYPE;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_DESKTOP;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_DOCK;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_TOOLBAR;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_MENU;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_UTILITY;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_SPLASH;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_DIALOG;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_DROPDOWN_MENU;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_POPUP_MENU;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_TOOLTIP;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_NOTIFICATION;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_COMBO;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_DND;
+		static xcb_atom_t _NET_WM_WINDOW_TYPE_NORMAL;
+
+		static xcb_atom_t _NET_WM_STATE;
+		static xcb_atom_t _NET_WM_STATE_STICKY;
+		static xcb_atom_t _NET_WM_STATE_MAXIMIZED_VERT;
+		static xcb_atom_t _NET_WM_STATE_MAXIMIZED_HORZ;
+		static xcb_atom_t _NET_WM_STATE_FULLSCREEN;
+		static xcb_atom_t _NET_WM_STATE_ABOVE;
+		static xcb_atom_t _NET_WM_STATE_BELOW;
+		static xcb_atom_t _NET_WM_STATE_DEMANDS_ATTENTION;
+		static xcb_atom_t _NET_WM_STATE_FOCUSED;
+
+		static xcb_atom_t _NET_WM_ALLOWED_ACTIONS;
+		static xcb_atom_t _NET_WM_ACTION_MOVE;
+		static xcb_atom_t _NET_WM_ACTION_RESIZE;
+		static xcb_atom_t _NET_WM_ACTION_STICK;
+		static xcb_atom_t _NET_WM_ACTION_MAXIMIZE_HORZ;
+		static xcb_atom_t _NET_WM_ACTION_MAXIMIZE_VERT;
+		static xcb_atom_t _NET_WM_ACTION_FULLSCREEN;
+		static xcb_atom_t _NET_WM_ACTION_CLOSE;
+		static xcb_atom_t _NET_WM_ACTION_ABOVE;
+		static xcb_atom_t _NET_WM_ACTION_BELOW;
+
+		static xcb_atom_t _NET_WM_STRUT_PARTIAL;
+		static xcb_atom_t _NET_FRAME_EXTENTS;
+
+		static xcb_atom_t ATOM;
+		static xcb_atom_t STRING;
+		static xcb_atom_t UTF8_STRING;
+
+		static void Initialize(xcb_connection_t *XConnection);
+	};
+}
+
+#endif
