f620da24801ee7eb18c2d819f320fb0239ba424b
Chris Foster <cdbfoster@gmail.com>
2015-05-13 11:42:45 -0400

    Revert "Added the Default_FrameWindow class."
    This reverts commit 9e7c70327f0005fa9bb11d422ad85a5f639108a3.

--- source/glass/windowdecorator/Default_WindowDecorator.cpp
+++ source/glass/windowdecorator/Default_WindowDecorator.cpp
@@ -18,7 +18,6 @@
 */
 
 #include "glass/core/DisplayServer.hpp"
-#include "glass/window/Default_FrameWindow.hpp"
 #include "glass/windowdecorator/Default_WindowDecorator.hpp"
 
 using namespace Glass;
@@ -50,35 +49,16 @@ void Default_WindowDecorator::DecorateWindow(ClientWindow &ClientWindow, unsigne
 	// Find the frame if it already exists
 	auto AuxiliaryWindowsAccessor = this->GetAuxiliaryWindows(ClientWindow);
 
-	for (auto AuxiliaryWindow = AuxiliaryWindowsAccessor->begin();
-			  AuxiliaryWindow != AuxiliaryWindowsAccessor->end();
-			  ++AuxiliaryWindow)
+	for (auto AuxiliaryWindow : *AuxiliaryWindowsAccessor)
 	{
-		if ((Frame = dynamic_cast<FrameWindow *>(*AuxiliaryWindow)))
-		{
-			if (dynamic_cast<Default_FrameWindow *>(Frame))
-				break;
-			else // Delete other types of frames
-			{
-				{
-					auto AuxiliaryWindowsAccessor = this->GetAuxiliaryWindows();
-
-					AuxiliaryWindowsAccessor->remove(Frame);
-				}
-
-				delete Frame;
-
-				Frame = nullptr;
-
-				AuxiliaryWindow = AuxiliaryWindowsAccessor->erase(AuxiliaryWindow);
-			}
-		}
+		if ((Frame = dynamic_cast<FrameWindow *>(AuxiliaryWindow)))
+			break;
 	}
 
 
 	if (Frame == nullptr)
 	{
-		Frame = new Default_FrameWindow(ClientWindow, "Frame", this->DisplayServer, FrameThickness * -1, FrameThickness, true);
+		Frame = new FrameWindow(ClientWindow, "Frame", this->DisplayServer, FrameThickness * -1, FrameThickness, true);
 
 		AuxiliaryWindowsAccessor->push_back(Frame);
 
